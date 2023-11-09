8713195a4b03068040232d8296c439ebdeb5cb04
xandervanboom <xander.van.boom@student.howest.be>
2023-11-09 10:06:09 +0100

    change sln name

---
+++ src/Common/Animal.cs
@@ -0,0 +1,11 @@
+namespace Common;
+
+public class Animal
+{
+    public string Name { get; set; }
+    public string Species { get; set; }
+    public int AgeInDays { get; set; }
+    public int WeightInGrams { get; set; }
+    public int HeightInCm { get; set; }
+    
+}
