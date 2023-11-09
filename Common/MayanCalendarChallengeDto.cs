b73e40a033385c102bd981ab091e9b44e23bb2de
xandervanboom <xander.van.boom@student.howest.be>
2023-11-09 10:06:01 +0100

    Edit sln name

--- Common/MayanCalendarChallengeDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Common;
-
-public record MayanCalendarChallengeDto
-{
-    public DateOnly StartDate { get; set; }
-    public DateOnly EndDate { get; set; }
-    public string Day { get; set; }
-}
--- Common/VineNavigationChallengeDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Common;
-
-public class VineNavigationChallengeDto
-{
-    public int AmountOfVines { get; set; }
-    public string Start { get; set; }
-    public string[] Directions { get; set; }
-}
